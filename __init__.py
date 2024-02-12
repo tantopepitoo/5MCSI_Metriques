b042a3353cafb48992b06daeba28bf6d4a1a9d06
tantopepitoo <159757626+tantopepitoo@users.noreply.github.com>
2024-02-12 09:40:08 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"  
+    return render_template("linkedin.html")
 
 @app.route('/paris/')
 def meteo():
