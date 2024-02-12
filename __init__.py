373b6d75e1bef9b8cec6fce0547aee5c2719fdc6
tantopepitoo <159757626+tantopepitoo@users.noreply.github.com>
2024-02-12 09:35:16 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,10 +26,6 @@ def meteo():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
-
-@app.route("/histogramme/")
-def mongraphique():
-    return render_template("histogramme.html")
   
 @app.route('/')
 def hello_world():
