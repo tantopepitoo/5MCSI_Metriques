87ecaeb6ddcf9d145051c459619a9be409a25a96
tantopepitoo <159757626+tantopepitoo@users.noreply.github.com>
2024-02-12 09:30:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,7 +29,7 @@ def mongraphique():
 
 @app.route("/histogramme/")
 def mongraphique():
-    return render_template("graphique.html")
+    return render_template("histogramme.html")
   
 @app.route('/')
 def hello_world():
